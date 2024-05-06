7dc9bcc6bfb6c11a653e40eb1b6c5144b8bf0767
Dave Banwell <dave@davebanwell.com>
2024-05-06 02:56:25 -0400

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Code your solution here
+function findMatching(list, criterion) {
+    return list.filter(listItem => listItem.toUpperCase() === criterion.toUpperCase());
+  }
+
+  function fuzzyMatch(list, criterion) {
+    return list.filter(listItem => listItem.substring(0,criterion.length).toUpperCase() === criterion.toUpperCase());
+  }
+
+  function matchName(list, criterion) {
+    return list.filter(listItem => listItem.name.toUpperCase() === criterion.toUpperCase());
+  }
